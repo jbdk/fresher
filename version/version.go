a445f94dddc1a90f8568301e81f7faebd7dc5317
c9845 (AA) <>
2022-10-25 13:17:08 -0400

    But version due to new config file EntryPoint field.

--- version/version.go
+++ version/version.go
@@ -8,7 +8,7 @@ package version
 // V is the version number of the app. This should match the git tag at the point this
 // version was released. This value is stored here, and not in main.go, so that we can
 // get it from any other package as needed (aka pages for diagnostic page).
-const V = "1.0.0-beta2"
+const V = "1.1.0"
 
 // ReleaseDate is the date this verison was released on.
-const ReleaseDate = "2022-09-10"
+const ReleaseDate = "2022-10-25"
